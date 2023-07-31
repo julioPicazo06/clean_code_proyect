cbd3d4bc35d8f49e069ecdb98c5c7dd658a43809
Jicazo <juliopicazo06@gmail.com>
2023-07-31 10:18:33 -0600

    feat(configurations): create the enviroments

---
+++ lib/infrastructor/moviedb_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:clean_code_proyect/config/constants/environment.dart';
+import 'package:clean_code_proyect/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+import '../domain/dataSources/movies_dataSource.dart';
+
+class MovieDBDataSourcetaSource extends MovieDataSource {
+  final dio = Dio(
+      BaseOptions(baseUrl: 'https://api.themoviedb.org/3', queryParameters: {
+    'api_key': Environments.theMovieKey,
+    'language': 'es-MX'
+  }, headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ${Environments.theMovieToken}',
+    'api_key': Environments.theMovieKey
+  }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final res = await dio.get(
+      '/movie/now_playing',
+    );
+
+    final List<Movie> movies = [];
+
+    return movies;
+  }
+}
